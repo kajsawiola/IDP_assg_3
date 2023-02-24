2b9aabc238c33e927ff11a336db1eadd3eaa8bdb
kajsawiola <kajsam@hotmail.com>
2023-02-24 09:21:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Dag_15_inlamning
+{
+    internal class Program
+    {
+
+    }
+    
+    class Element
+    {
+        public string namn, Z, typ, smältpunkt, kokpunkt;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
