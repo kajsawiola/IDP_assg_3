1791beca796b3226c6d5d2cc0481656434d973e4
kajsawiola <kajsam@hotmail.com>
2023-02-24 09:27:34 +0100

    Lagt till grundämnena syre, järn och guld.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,31 @@
         public string namn, Z, typ, smältpunkt, kokpunkt;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Element syre = new Element()
+            {
+                namn = "syre",
+                Z = "8",
+                typ = "ickemetall",
+                smältpunkt = "54.36",
+                kokpunkt = "90.188"
+            };
+            
+            Element järn = new Element()
+            {
+                namn = "järn",
+                Z = "26",
+                typ = "metall",
+                smältpunkt = "1811",
+                kokpunkt = "3134"
+            };
+            Element guld = new Element()
+            {
+                namn = "guld",
+                Z = "79",
+                typ = "metall",
+                smältpunkt = "1337.33",
+                kokpunkt = "3243"
+            };
         }
     }
 }
